a37ec87652f1a45abd5fdab15f10d9483f4c65eb
Alexis Franco <ronaldofranco196@gmail.com>
2023-02-26 19:22:45 -0300

    creación Index

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LayoutHeader from '../../layouts/LayoutHeader/LayoutHeader'
+import Aside from '../Aside/Aside'
+
+export default function Hero() {
+    return (
+        <div>
+            <LayoutHeader/>
+            <Aside/>
+        </div>
+)
+}
