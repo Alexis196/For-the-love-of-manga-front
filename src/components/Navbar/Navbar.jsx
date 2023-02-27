603499b66d8f5d778f8dccee06ca59abd35cc9bc
Alexis Franco <ronaldofranco196@gmail.com>
2023-02-27 10:39:30 -0300

    Layout

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,15 +1,51 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './navbar.css'
 
-export default function Navbar() {
-    let menu = <img src='./img/Menu.svg' alt='Menu'/>
-    let logo = <img className='logo1' src='./img/Logo.svg' alt='Logo'/>
+
+const Navbar = () => {
+    let profile = <img className='profile-img' src='./img/profile.png' alt='profile'/>
+    let logo = <img className='logo' src='./img/logo.svg' alt='logo'/>
     let logo2 = <img className='logo2' src='./img/logo2.png' alt='logo2'/>
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-    <div className='navbar'>
-        {menu}
-        {logo}
-        {logo2}
-    </div>
-)
+
+        <nav>
+            <div className='navbar'>
+
+                <div className={`nav_items ${isOpen && "open"}`}>
+                    <div className='porfile'>
+
+                        {profile}
+
+                        <div className='porfile-text'>
+                            <p>Lucas Ezequiel Silva</p>
+                            <p>lucasezequielsilva@gmail.com</p>
+                        </div>
+
+                    </div>
+
+                    <a href="#"><span>Home</span></a>
+                    <a href="#">Mongos</a>
+                    <a href="#">My mongos</a>
+                    <a href="#">Favourites</a>
+                    <a href="#">Logout</a>
+                </div>
+                <div className={`nav_toggle  ${isOpen && "open"}`} onClick={() => setIsOpen(!isOpen)}>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+
+            </div>
+
+            <div className='logo'>
+                {logo}
+                {logo2}
+            </div>
+        </nav>
+    )
+
 }
+
+export default Navbar
