09a49d87108584a569ab4ff3f6a9f4af5fb83d39
Alexis Franco <108986498+Alexis196@users.noreply.github.com>
2023-02-27 10:45:47 -0300

    M 02 (#5)
    * creación form
    
    * M 02 form (#4)
    
    * diseño y estructura del form
    
    * formulario funcional
    
    * errores
    
    * corrección errores CSS

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import logo from './logo.svg';
 import './App.css';
 import LayoutHeader from './layouts/LayoutHeader/LayoutHeader';
 import Aside from './components/Aside/Aside';
+import Register from './components/Register/Register'
 
 function App() {
   return (
     <div>
       <LayoutHeader className='header'/>
       <Aside/>
+      <Register/>
     </div>
   );
 }
--- src/components/DivFondos/DivFondo.jsx
+++ src/components/DivFondos/DivFondo.jsx
@@ -6,7 +6,7 @@ export default function DivFondo() {
         <div className='div-fondo'>
             <h1 className='h1'>For the love of manga</h1>
             <p className='text1'>Explore our varieties</p>
-            <p className='text2'>#MingaLove❤</p>
+            <p className='text2'>#MingaLove <span className='span-cor'>❤</span> </p>
             <button className='btn-primary'><span>Let’s go!</span></button>
         </div>
     
