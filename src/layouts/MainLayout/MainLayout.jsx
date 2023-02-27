c9d91ae42daaccc13faf1c656fbbf3b0c9dd00d5
Alexis Franco <108986498+Alexis196@users.noreply.github.com>
2023-02-27 12:21:47 -0300

    M 04 (#9)
    * creación form
    
    * diseño y estructura del form
    
    * formulario funcional
    
    * creación Index
    
    * M 04 dev (#8)
    
    * Layout
    
    * correción CSS, IndexLayout

---
+++ src/layouts/MainLayout/MainLayout.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Footer from '../../components/Footer/Footer'
+import Navbar from '../../components/Navbar/Navbar'
+
+export default function MainLayout() {
+    return (
+    <div>
+        <Navbar/>
+        <Footer/>
+    </div>
+)
+}
